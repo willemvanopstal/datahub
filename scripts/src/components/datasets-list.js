5abf64a9979ef1fc901571017239b6912bd4f0de
willemvanopstal <willemvanopstal@home.nl>
2018-03-06 12:36:19 +0100

    re search

--- scripts/src/components/datasets-list.js
+++ scripts/src/components/datasets-list.js
@@ -55,7 +55,7 @@ export default class {
   // Returns a function that can be used to search an array of datasets
   // The function returns the filtered array of datasets
   _createSearchFunction (datasets) {
-    const keys = ['title', 'notes', 'searchcats']
+    const keys = ['title', 'notes', 'searchcats', 'searchrec']
     return function (query) {
       const lowerCaseQuery = query.toLowerCase()
       return filter(datasets, function (dataset) {
